5a10a26cf3808d1366a25f613ec24faf3b0b252e
Ng Wei En <weien.ng.2016@sis.smu.edu.sg>
2018-03-25 16:20:53 +0800

    Updated loanAmount

---
+++ LoanWebApplication/web/LoanWebAppServlet.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.*;
+import java.util.*;
+
+/**
+ *
+ *
+ */
+@WebServlet(name = "LoanWebAppServlet", urlPatterns = {"/json"})
+public class LoanWebAppServlet extends HttpServlet {
+
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            ArrayList<String> result = new ArrayList<String>();
+            Gson gson = new GsonBuilder().create();
+
+            String fullName = request.getParameter("fullName");
+            String nric = request.getParameter("NRIC");
+            String loanAmount = request.getParameter("loanAmount");
+            String loanType = request.getParameter("loanType");
+
+            if (fullName!= null && !fullName.equals("") &&
+                    nric!= null && !nric.equals("") &&
+                            loanAmount!= null && !loanAmount.equals("") &&
+                              loanType!= null && !loanType.equals("")) {
+               
+              
+
+            out.println(gson.toJson(responseMap));
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
