d59b0e5669cbb17e8e8d55422d171218736779ed
Unknown <exoforcegit@gmail.com>
2018-03-30 01:22:06 +0800

    Insert the blacklist database
    names and nric

---
+++ Database/Blacklist/blacklist.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM blacklist.personal_details
+LIMIT 0, 1000
+
+-- Date: 2018-03-30 01:20
+*/
+INSERT INTO `personal_details` (`nric`,`name`) VALUES ('S1234567A','Nathan Neo');
+INSERT INTO `personal_details` (`nric`,`name`) VALUES ('S2345678B','Gabriel Kwan');
